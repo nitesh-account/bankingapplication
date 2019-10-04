3d636dc48ccfc01cf16354322dfd7944c84a4407
Nitesh Kumar <nitesh@sdglobaltech.com>
2019-10-05 00:13:07 +0800

    Added code

--- src/main/java/com/bankingapplication/rest/domain/Transaction.java
+++ src/main/java/com/bankingapplication/rest/domain/Transaction.java
@@ -33,11 +33,11 @@ public class Transaction extends BaseMaster implements Serializable {
     @Column(name="TRANSACTION_AMOUNT")
     private Double amount;
 
-    public Integer getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(String id) {
         this.id = id;
     }
 
